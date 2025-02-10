47791d659e4c3f40106eb50be89661510dfec395
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-10 19:13:38 +0100

    Update main_02.py

--- main_02.py
+++ main_02.py
@@ -22,7 +22,19 @@ while True:
         # TODO Napraviti funkcionalnost dohvata i prikaza svih zadataka
         # Implementirati ovaj dio koda tako da se ispise 10 zadataka na nacin:
         # Zadatak 1. - Ovo je tekst 1. zadatka!
-        pass
+
+        # Golubovic - for petlja
+        # for i in range(1, 11):
+        #     print(f'Zadatak {i}. - Ovo je tekst {i}. zadatka!')
+
+        # Vojic - while petlja
+        i = 1
+        while i < 11:
+            print(f'Zadatak {i}. - Ovo je tekst {i}. zadatka!')
+            # i = i + 1
+            i += 1 # Ovo je identicno liniji iznad, ali je kraci zapis.
+
+        # pass
 
     elif choice == '3':
         break
