bd275cf51d9ead66e37cbfd2d0ff32f6765453ad
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-10 19:47:59 +0100

    Update main_04.py

---
+++ main_04.py
@@ -0,0 +1,13 @@
+# Napisite program koji omogucava korisniku unos kontakata (ime i prezime; email; telefon),
+# koliko god to korisnik zeli.
+
+while True:
+
+    # forma za unos kontakta
+
+
+    # odluka unosa novog kontakta
+
+
+
+# Odjavna poruka
