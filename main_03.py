b826b9d7f6975225dbf9fb0280ec9b8f9aae8374
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-10 19:23:56 +0100

    Update main_03.py

--- main_03.py
+++ main_03.py
@@ -1,2 +1,31 @@
 # Ispišite brojeve od 1 do 10 koristeći se prvo for, a onda while petljom.
 
+# Srđan Golubović
+
+print("For loop:")
+for i in range (1, 11):
+    print(i)
+
+print("Di je moja nagrada!")
+print()
+
+print("While loop:")
+
+i = 1
+while i < 11:
+    print(i)
+    i = i + 1
+
+print('Srđan Golubović:', "Di je moja nagrada!")
+
+print('Krunoslav Lujić:', 'Nagrada je poslana na Vasu postu, kurirskom sluzbom 24Paket!')
+print('Krunoslav Lujić:', '!!!Nagrada!!!')
+
+print('')
+print('    ()     ()')
+print('    ||     ||    ')
+print('-----------------')
+print('|||||||||||||||||')
+print('-----------------')
+print('|||||||||||||||||')
+print('-----------------')
